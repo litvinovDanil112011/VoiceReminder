3296bea7be503502c1204ff8b2b68962a19590c4
litvinovDanil112011 <danil112011@mail.ru>
2023-05-02 14:16:00 +0300

    Initial Commit

---
+++ VoiceReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceReminder
+//
+//  Created by Daniil Litvinov on 02.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
